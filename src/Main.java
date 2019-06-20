3b12e5bf0577a9b7f614572ab8155e9b06c2627e
Maksim <maximus.n@gmail.com>
2019-06-20 10:11:58 +0300

    this file going to be optimized

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,41 @@
 import static java.lang.System.out;
 
-public class Main {
+    import java.math.BigDecimal;
+    import java.util.Scanner;
 
-    public static void main(String[] args) {
+    public class Main {
 
-        System.out.println("Hi");
-    }
+        public static void main(String[] args) {
+
+            BigDecimal weight;
+            BigDecimal distance;
+
+            BigDecimal pricePerKg = new BigDecimal("30");
+            BigDecimal pricePerKm = new BigDecimal("50");
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the weight in kilograms or 'q' for quit: ");
+            String inputString = scanner.nextLine();
+            if (!inputString.equals("Q")) {
+                weight = new BigDecimal(inputString);
+            } else {
+                System.out.println("\nBye!");
+                return;
+            }
+
+            System.out.println("Enter the distance in kilometers or 'q' for quit: ");
+            inputString = scanner.nextLine();
+            if (!inputString.equals("Q")) {
+                distance = new BigDecimal(inputString);
+            } else {
+                System.out.println("\nBye!");
+                return;
+            }
+
+            System.out.println("Value of weight = " + weight);
+            System.out.println("Value of distance = " + distance);
+
+            BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
+            System.out.println("Price = " + price);
+        }
 }
